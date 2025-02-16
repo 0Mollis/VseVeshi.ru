91fd20b7f30b8060508271fb0dfb4eecb388d741
0Mollis <maks.chudnov.05@mail.ru>
2025-02-16 14:16:09 +0300

    Добавьте файлы проекта.

---
+++ VseVeshi.ru/Pages/Item.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ВсеВещи.ру.Pages
+{
+    public class ItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ VseVeshi.ru/wwwroot/js/site.js
@@ -0,0 +1,61 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function scrollCatalog(direction) {
+    let scrollAmount = 0;
+    let catalog = document.querySelector('.catalog');
+
+    let itemWidth = catalog.querySelector('.catalog-item').offsetWidth + 35.2; // 35.2 - это margin
+    scrollAmount += direction * itemWidth;
+    // Защита от прокрутки за границы
+    let maxScroll = catalog.scrollWidth - catalog.parentNode.clientWidth;
+    if (scrollAmount < 0) {
+        scrollAmount = 0;
+    } else if (scrollAmount > maxScroll) {
+        scrollAmount = maxScroll;
+    }
+    // Устанавливаем новое положение каталога
+    catalog.style.transform = `translateX(-${scrollAmount}px)`;
+}
+check_quantity();
+function quantity_plus() {
+    var quantity = document.querySelector('.quantity-col');
+    var maxquantity = document.querySelector('.maxquantity');
+    quantity.textContent = Number(quantity.textContent) + 1;
+    check_quantity();
+}
+
+function check_quantity() {
+    var quantity = document.querySelector('.quantity-col');
+    var btn = document.querySelector('.button-quantity')
+    var mbtn = document.querySelector('.mbutton-quantity')
+    var maxquantity = document.querySelector('.maxquantity');
+    if (Number(quantity.textContent) + 1 > maxquantity.textContent) {
+        btn.disabled = true;
+        btn.classList.remove('quantity-plus');
+        btn.classList.add('quantity-min');
+    }
+    else {
+        btn.disabled = false;
+        btn.classList.add('quantity-plus')
+    }
+    if (Number(quantity.textContent) - 1 < 1) {
+        mbtn.disabled = true;
+        mbtn.classList.remove('quantity-plus');
+        mbtn.classList.add('quantity-min');
+    }
+    else {
+        mbtn.disabled = false;
+        mbtn.classList.add('quantity-plus')
+    }
+}
+
+function quantity_minus() {
+    var quantity = document.querySelector('.quantity-col');
+    var maxquantity = document.querySelector('.maxquantity');
+    quantity.textContent = Number(quantity.textContent) - 1;
+    check_quantity();
+}
