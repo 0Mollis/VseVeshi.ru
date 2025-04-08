d529022beb53fad3f6c40207b4811717fa0748ea
0Mollis <maks.chudnov.05@mail.ru>
2025-04-08 12:12:25 +0300

    Корзина (отображение таблицы)

--- VseVeshi.ru/Pages/Cart.cshtml.cs
+++ VseVeshi.ru/Pages/Cart.cshtml.cs
@@ -1,12 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using VseVeshi.ru.Data;
+using VseVeshi.ru.Models;
 
 namespace VseVeshi.ru.Pages
 {
     public class CartModel : PageModel
     {
+        private ApplicationDbContext _context;
+        private UserManager<IdentityUser> _userManager;
+
+        public CartModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public List<Cart> carts;
+
         public void OnGet()
         {
+            carts = _context.carts.Include(c => c.RentItems).Include(c => c.User).Where(x => x.User != null && x.User.Id == _userManager.GetUserId(User)).ToList();
         }
     }
 }
